c1a8612248bcf183aa84d476438e5d5bdbf415c0
794035516 <794035516@qq.com>
2022-11-21 15:48:04 +0800

    format code and edit xiaour to lijx yes, this demo is copied by others( https://github.com/xiaour/SpringBootDemo/tree/master/SpringBootRocketMqDemo )

--- src/main/java/com/lijx/rocketmq/SpringbootRocketmqApplication.java
+++ src/main/java/com/lijx/rocketmq/SpringbootRocketmqApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootRocketmqApplication {
+public class SpringbootRocketmqApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringbootRocketmqApplication.class, args);
 	}
 
--- src/main/java/com/lijx/rocketmq/producer/Producer.java
+++ src/main/java/com/lijx/rocketmq/producer/Producer.java
@@ -16,13 +16,14 @@ import javax.annotation.PostConstruct;
 import java.io.UnsupportedEncodingException;
 
 /**
- * @Author: Xiaour
+ * @Author: lijx
  * @Description:生产者
- * @Date: 2018/8/9 14:52
+ * @Date: 2022/11/21
  */
 
 @Component
-public class Producer {
+public class Producer
+{
 
     /**
      * 生产者的组名
@@ -38,29 +39,34 @@ public class Producer {
     private String namesrvAddr;
 
     @PostConstruct
-    public void defaultMQProducer() {
+    public void defaultMQProducer()
+    {
 
         //生产者的组名
-        producer= new DefaultMQProducer(producerGroup);
+        producer = new DefaultMQProducer(producerGroup);
         //指定NameServer地址，多个地址以 ; 隔开
         producer.setNamesrvAddr(namesrvAddr);
         producer.setVipChannelEnabled(false);
-        try {
+        try
+        {
             producer.start();
             System.out.println("-------->:producer启动了");
-        } catch (MQClientException e) {
+        } catch (MQClientException e)
+        {
             e.printStackTrace();
         }
     }
 
-    public String send(String topic,String tags,String body) throws InterruptedException, RemotingException, MQClientException, MQBrokerException, UnsupportedEncodingException {
+    public String send(String topic, String tags, String body)
+            throws InterruptedException, RemotingException, MQClientException, MQBrokerException, UnsupportedEncodingException
+    {
         Message message = new Message(topic, tags, body.getBytes(RemotingHelper.DEFAULT_CHARSET));
         StopWatch stop = new StopWatch();
         stop.start();
         SendResult result = producer.send(message);
         System.out.println("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
         stop.stop();
-        return "{\"MsgId\":\""+result.getMsgId()+"\"}";
+        return "{\"MsgId\":\"" + result.getMsgId() + "\"}";
     }
 
 }
